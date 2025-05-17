ca1ce0e3b04b8fb294cc686590491b3585b33048
denisha-madhura <denisha.madhura2007@gmail.com>
2025-05-18 00:51:18 +0530

    changes to diceRoller and added rock paper scissors

---
+++ learnJava/src/slotMachine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class slotMachine {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        System.out.println("W");
+
+        scanner.close(); 
+    }
+}
